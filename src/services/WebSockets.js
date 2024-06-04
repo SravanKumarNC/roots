d434b9bc33f6a494614d4414cd719dc6c37c7803
SravanKumarNC <sravankumarnc@gmail.com>
2024-06-04 11:54:32 +0530

    update

--- src/services/WebSockets.js
+++ src/services/WebSockets.js
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 const sendData = (message) => {
-  const ws = new WebSocket("wss://192.168.2.22:8765");
+  const ws = new WebSocket("wss://192.168.2.93:8765");
 
   ws.onopen = () => {
     console.log("connected to WebSocket server");
